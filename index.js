bc8ac2d779d8a324b85f47985c4e60ccd2e18914
Jahid Hasan <jahid.hasan@pmamediagroup.com>
2022-08-10 21:38:55 +0600

    cleaned

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const { createJsApp } = require("./AppController/jsApp");
 const { validateAppName, isNameAvailable } = require("./helper/validate");
 
 const appName = process.argv[2];
-if(appName && isNameAvailable(appName)){
-  console.log(`<<<<<<< ${appName} already exist in current directory, use different name >>>>>>>>`);
+if (appName && validateAppName(appName) && isNameAvailable(appName)) {
+  console.log(
+    `<<<<<<< ${appName} already exist in current directory, use different name >>>>>>>>`
+  );
   console.log("-----------npx koa-app-boilerplate appName----------");
-}
-else if (validateAppName(appName) && String(process.argv[3]).toLocaleLowerCase() !== "ts") {
+} else if (
+  validateAppName(appName) &&
+  String(process.argv[3]).toLocaleLowerCase() !== "ts"
+) {
   createJsApp(appName);
 } else {
-    console.log("<<<<<<< 'appName' Only alphabates are allowed >>>>>>>>");
-    console.log("----------- npx koa-app-boilerplate appName ----------");
-
+  console.log("<<<<<<< 'appName' Only alphabates are allowed >>>>>>>>");
+  console.log("----------- npx koa-app-boilerplate appName ----------");
 }
-// execSync(`node index.js`,  { cwd: `${arg[2]}/src` });
